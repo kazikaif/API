da155cab9c70960bac83a5bab807f478938e4e18
Kazi Mohammad Kaif <kaifkazi83@gmail.com>
2025-03-23 15:22:36 +0530

    Bitcoin

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const API_URL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
+let Crptodata = [];
 async function FetchData() {
   try {
     const response = await fetch(API_URL);
     const data = await response.json();
+    Crptodata = data;
     displayData(data);
     console.log(data);
   } catch (error) {
@@ -15,30 +17,47 @@ async function FetchData() {
 function displayData(data) {
   const tbody = document.getElementById("tbody");
 
-  tbody.innerHTML = ""; 
+  tbody.innerHTML = "";
 
   data.forEach((coin) => {
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td><img src="${coin.image}" alt="${coin.name}" width="30"/></td>
-      <td>${coin.symbol.toUpperCase()}</td>
-      <td>${coin.name}</td>
-      <td>$${coin.current_price.toLocaleString()}</td>
-      <td>$${coin.market_cap.toLocaleString()}</td>
-      <td style="color: ${coin.price_change_percentage_24h >= 0 ? "green" : "red"}">
-        ${coin.price_change_percentage_24h.toFixed(2)}%
-      </td>
-    `;
+        <td><img src="${coin.image}" alt="${coin.name}" width="30"/></td>
+        <td>${coin.symbol.toUpperCase()}</td>
+        <td>${coin.name}</td>
+        <td>$${coin.current_price.toLocaleString()}</td>
+        <td>$${coin.market_cap.toLocaleString()}</td>
+        <td style="color: ${
+          coin.price_change_percentage_24h >= 0 ? "green" : "red"
+        }">
+          ${coin.price_change_percentage_24h.toFixed(2)}%
+        </td>
+      `;
     tbody.appendChild(row);
   });
 }
 
+function Search() {
+  const serachinput = document.getElementById("Sh").value.toLowerCase();
 
-function Search(){
-  let searchinput = document.getElementById("search").ariaValueMax.toLocaleLowerCase();
-   let fillter = arr.fillter((coin)=>
-    coin.name.toLocaleLowerCase().includes(searchinput)
-   );
- }
+  const filterdata = Crptodata.filter(
+    (coin) =>
+      coin.name.toLowerCase().includes(serachinput) ||
+      coin.symbol.toLowerCase().includes(serachinput)
+  );
+  displayData(filterdata);
+}
 
+function Cap() {
+  const CapSorted = [...Crptodata].sort((a, b) => b.market_cap - a.market_cap);
+
+  displayData(CapSorted);
+}
+
+function Percentage() {
+  const PercentageSorted = [...Crptodata].sort(
+    (a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h
+  );
+  displayData(PercentageSorted);
+}
 FetchData();
